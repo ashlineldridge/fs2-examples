276ff08556a9eb17772ac62427cf187e34bcf4ee
Ashlin Eldridge <aeldridge@seek.com.au>
2018-03-23 15:47:56 +1100

    Fleshed out thread shifting example

--- src/main/scala/com/example/concurrency/ThreadShifting.scala
+++ src/main/scala/com/example/concurrency/ThreadShifting.scala
@@ -1,25 +1,58 @@
 package com.example.concurrency
 
-import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{Executors, ThreadFactory}
 
 import cats.effect.IO
 import com.typesafe.scalalogging.LazyLogging
+import fs2._
 
 import scala.concurrent.ExecutionContext
 
-// Not FS2 specific but an example of shifting threads -
-// idea taken from here: https://typelevel.org/blog/2017/05/02/io-monad-for-cats.html
+// Idea taken from here: https://typelevel.org/blog/2017/05/02/io-monad-for-cats.html
 object ThreadShifting extends App with LazyLogging {
 
-  val DefaultThreadPool = ExecutionContext.fromExecutor(null)
-  val BlockingIoThreadPool = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+  implicit val DefaultThreadPool = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2, new NamedThreadFactory("default-thread-pool-")))
+  val BlockingIoThreadPool = ExecutionContext.fromExecutor(Executors.newCachedThreadPool(new NamedThreadFactory("blocking-io-thread-pool-")))
 
-  (for {
-    _ <- IO(logger.info("Main thread"))
-    _ <- IO.shift(BlockingIoThreadPool)
-    _ <- IO(logger.info("Some long running IO operation..."))
-    _ <- IO(Thread.sleep(2000))
-    _ <- IO.shift(DefaultThreadPool)
-    _ <- IO(logger.info("Some CPU bound operation"))
-  } yield ()).unsafeRunSync
+  class Worker(id: Int) {
+    def run: Stream[IO, Unit] =
+      Stream.eval(
+        for {
+          _ <- IO(logger.info(s"#${id}: Starting..."))
+          _ <- IO.shift(BlockingIoThreadPool)
+          _ <- IO(logger.info(s"#${id}: Some long running IO operation..."))
+          _ <- IO(Thread.sleep(2000))
+          _ <- IO.shift(DefaultThreadPool)
+          _ <- IO(logger.info(s"#${id}: Some CPU bound operation"))
+          _ <- IO(Thread.sleep(500))
+        } yield ())
+  }
+
+  Stream.range(1, 10)
+    .map(new Worker(_))
+    .map(_.run)
+    .join(3)
+    .compile
+    .drain
+    .flatMap(_ => IO(logger.info("Done")))
+    .unsafeRunSync
+}
+
+// Adapted from Executors.DefaultThreadFactory so that we can control
+// thread naming to more easily distinguish the thread pool from logging
+class NamedThreadFactory(namePrefix: String) extends ThreadFactory {
+
+  private val securityManager = System.getSecurityManager
+  private val threadNumber = new AtomicInteger(1)
+  private val threadGroup: ThreadGroup =
+    if (securityManager != null) securityManager.getThreadGroup
+    else Thread.currentThread.getThreadGroup
+
+  override def newThread(r: Runnable): Thread = {
+    val t = new Thread(threadGroup, r, namePrefix + threadNumber.getAndIncrement, 0)
+    if (t.isDaemon) t.setDaemon(false)
+    if (t.getPriority != Thread.NORM_PRIORITY) t.setPriority(Thread.NORM_PRIORITY)
+    t
+  }
 }
